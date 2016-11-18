12eef585c27ecbff0d1057ec1548ad1348b497d7
Christian Vetter <christian@rasenplanscher.info>
2016-11-18 20:29:23 +0100

    add cases waiting for ready

---
+++ chokidar-afterReady.js
@@ -0,0 +1,21 @@
+var chokidar = require('chokidar')
+
+console.log('init')
+
+// configuration taken from webpack
+var watcher = chokidar.watch('.', {
+  ignoreInitial: true,
+  persistent: true,
+  followSymlinks: false,
+  depth: 0,
+  atomic: false,
+  alwaysStat: true,
+  ignorePermissionErrors: true
+})
+
+console.log('watching')
+
+watcher.on('ready', () => {
+  watcher.close()
+  console.log('closed')
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp')
 
 gulp.task('default', done => {
-  gulp.watch('.').close();
-  done();
+  gulp.watch('.').close()
+  done()
+})
+
+gulp.task('works', done => {
+  var watcher = gulp.watch('.')
+
+  watcher.on('ready', watcher.close)
+
+  done()
 })
 
